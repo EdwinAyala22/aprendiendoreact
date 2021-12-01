fa954037ed93662f5abaf3f6a076289460ea27f7
Edwin Ayala <e22ayalarecalde@gmail.com>
2021-11-30 23:05:10 -0500

    Se modificaron los archivos de App.js, App.css, index.html, se crearon nuevos componentes y un nuevo archivo .json

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
+import React, {Component} from 'react';
 import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 
+
 //Importar componentes
 
-import MiComponente from './components/MiComponente';
+import  {todos}  from './todos.json';
 
-function HolaMundo(nombre, edad){
-  var presentacion = (
-    <div>
-      <h2> Hola, soy {nombre}</h2>
-      <h3>Tengo {edad} años</h3>
-    </div>
-  );
-  
-  return presentacion
-}
-
-
-function App() {
-  var nombre = "Edwin Ayala Recalde";
- 
-  return (
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos
+    }
+  }
+  render(){
+    const todos = this.state.todos.map((todo, i) =>{
+      return(
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge rounded-pill bg-danger mx-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p><mark>{todo.responsible}</mark></p>
+            </div>
+          </div>
+        </div>
+      )  
+    })
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola, bienvenido al curso de React de Victor Robles WEB!!
-        </p>
-        {HolaMundo(nombre, 18)}
-        <section className="componetes">
+      <nav className="navbar navbar-dark bg-dark">
+          <a href="https://www.google.com/?hl=es" className ="text-white">
+            Tasks
+          </a>
+          <span className="badge rounded-pill bg-light text-dark m-2">
+              { this.state.todos.length }
+          </span>
+      </nav>
 
-          <MiComponente />
+      <div className="container">
+        <div className="row mt-4">
+        { todos }
+        </div>
+      </div>
 
-        </section>
-      </header>
+      <img src={logo} className="App-logo" alt="logo"></img>
       
     </div>
   );
 }
+}
+
+/*function App() {
+  constructor(props) {
+    super(props);
+  }
+  return (
+    <div className="App">
+      <nav className="navbar navbar-dark bg-dark">
+          <a className="text-white">
+            Tasks
+          </a>
+      </nav>
+      <img src={logo} className="App-logo" alt="logo"></img>
+      
+    </div>
+  );
+}*/
 
 export default App;
